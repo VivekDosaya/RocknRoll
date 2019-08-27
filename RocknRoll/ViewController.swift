6e2488a43651364f8ca05b0bc5a6f4e92b96b622
VivekD <vkdosaya@users.noreply.github.com>
2019-08-27 09:55:56 +0530

    code made more readable!

--- RocknRoll/ViewController.swift
+++ RocknRoll/ViewController.swift
@@ -12,21 +12,19 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var dice2: UIImageView!
     @IBOutlet weak var dice1: UIImageView!
-    var x : Int = 0
-    var y : Int = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateDiceImage()
+    }
+    func updateDiceImage(){
+        dice1.image = UIImage(named: "dice\(Int.random(in: 1...6))")
+        dice2.image = UIImage(named: "dice\(Int.random(in: 1...6))")
+        
     }
 
     @IBAction func buttonPressed(_ sender: UIButton) {
-        x = Int.random(in: 1...6)
-        y = Int.random(in: 1...6)
-        dice1.image = UIImage(named: "dice\(x)")
-        dice2.image = UIImage(named: "dice\(y)")
-        
-        
-        
+        updateDiceImage()
     }
     
 }
