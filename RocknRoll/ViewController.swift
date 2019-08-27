6f221135d8e16c572e2fe857b72310dea5400aa2
VivekD <vkdosaya@users.noreply.github.com>
2019-08-27 09:52:28 +0530

    image view getting updated with the corresponding dice image as of the random number

--- RocknRoll/ViewController.swift
+++ RocknRoll/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dice2: UIImageView!
+    @IBOutlet weak var dice1: UIImageView!
+    var x : Int = 0
+    var y : Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        x = Int.random(in: 1...6)
+        y = Int.random(in: 1...6)
+        dice1.image = UIImage(named: "dice\(x)")
+        dice2.image = UIImage(named: "dice\(y)")
+        
+        
+        
+    }
+    
 }
 
